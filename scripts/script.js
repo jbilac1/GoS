58a2a21d7c2278f4f22bdee57555d3c68e1c2896
Jbilac <79338162+jbilac1@users.noreply.github.com>
2024-10-21 19:18:10 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,6 @@
-const hambi = document.querySelector('.hamburger');
+const hambi = document.querySelector('.hambi');
 const links = document.querySelector('.links');
-const span1 = document.getElementById('span1');
-const span2 = document.getElementById('span2');
-const span3 = document.getElementById('span3');
-const nav = document.querySelector('.navbar');
 
 hambi.addEventListener('click', ()=>{
-    nav.classList.toggle('show');
     links.classList.toggle('open');
-    span1.classList.toggle('r1');
-    span2.classList.toggle('r2');
-    span3.classList.toggle('r3');
 })
